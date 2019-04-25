0cea5d55cf0e4f33d8c9435300957ba9243b6546
DisjunkteMenge <elies.mabrouki@gmail.com>
2019-04-25 22:32:43 +0200

    Repeat programm and fix crash when wrong selection

--- main.c
+++ main.c
@@ -8,36 +8,50 @@ int main() {
     int zahl1;
     int zahl2;
     int auswahl;
+    int again;
+
+
+
+    do {
 
     printf("Waehle aus:\n1: Bruch addieren\n2: Bruch subtrahieren\n3: Bruch multiplizieren\n4: Bruch dividieren\n5: Bruch in Fliesskommadarstellung\n6: Bruch kuerzen\n7: kgV\n8: ggT\n");
     printf("Ihre Auswahl: ");
     scanf("%d", &auswahl);
 
-    if (auswahl == 1) {
-        addiere(b, c);
-    } else if (auswahl == 2) {
-        subtrahiere(b, c);
-    } else if (auswahl == 3) {
-        multipliziere(b, c);
-    } else if (auswahl == 4) {
-        dividiere(b, c);
-    } else if (auswahl == 5) {
-        toFloat(b);
-    } else if (auswahl == 6) {
-        kuerzeOnly(b);
-    } else if (auswahl == 7) {
-        printf("Zahl 1 : ");
-        scanf("%d", &zahl1);
-        printf("Zahl 2 : ");
-        scanf("%d", &zahl2);
-        printf("ggT( %d, %d) = %u\n", zahl1, zahl2, ggT(zahl1, zahl2));
-    }else if (auswahl == 8){
-        printf("Zahl 1 : ");
-        scanf("%d", &zahl1);
-        printf("Zahl 2 : ");
-        scanf("%d", &zahl2);
-        printf("kgV( %d, %d) = %u\n", zahl1, zahl2, kgV(zahl1, zahl2));
-    }
+        if (auswahl == 1) {
+            addiere(b, c);
+        } else if (auswahl == 2) {
+            subtrahiere(b, c);
+        } else if (auswahl == 3) {
+            multipliziere(b, c);
+        } else if (auswahl == 4) {
+            dividiere(b, c);
+        } else if (auswahl == 5) {
+            toFloat(b);
+        } else if (auswahl == 6) {
+            kuerzeOnly(b);
+        } else if (auswahl == 7) {
+            printf("Zahl 1 : ");
+            scanf("%d", &zahl1);
+            printf("Zahl 2 : ");
+            scanf("%d", &zahl2);
+            printf("ggT( %d, %d) = %u\n", zahl1, zahl2, ggT(zahl1, zahl2));
+        } else if (auswahl == 8) {
+            printf("Zahl 1 : ");
+            scanf("%d", &zahl1);
+            printf("Zahl 2 : ");
+            scanf("%d", &zahl2);
+            printf("kgV( %d, %d) = %u\n", zahl1, zahl2, kgV(zahl1, zahl2));
+        }else{
+            return 0;
+        }
+        printf("Repeat press 1, Exit press any key : ");
+        scanf("%d", &again);
+    } while (again == 1);
+
+
+
+
 
 
 
